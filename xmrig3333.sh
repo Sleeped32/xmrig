a8cf40a42d506713f7471c5e1a8b76599bea3ce4
Sleeped <114930693+Sleeped32@users.noreply.github.com>
2024-12-11 20:12:52 +0000

    Update xmrig3333.sh

--- xmrig3333.sh
+++ xmrig3333.sh
@@ -1,4 +1,4 @@
-#!/bin/zsh
+#!/bin/bash
 
 
-sudo xmrig --url pool.hashvault.pro:3333 --user 4514XVERAtaWYS4wjJTqXN4hsPJcGYytLR5vKky1k32VD9Vaun47e2VCcAfPVFjFLRTzgrfSeUXxvN1dkucJbekjJiAX9As --pass HP3333 --donate-level 1 --tls --tls-fingerprint 420c7850e09b7c0bdcf748a7da9eb3647daf8515718f36d9ccfdd6b9ff834b14 --cpu-no-yield --randomx-1gb-pages --log-file="xmrig.log" -B
+sudo xmrig --url pool.hashvault.pro:3333 --user 4514XVERAtaWYS4wjJTqXN4hsPJcGYytLR5vKky1k32VD9Vaun47e2VCcAfPVFjFLRTzgrfSeUXxvN1dkucJbekjJiAX9As --pass HP3333 --donate-level 1 --tls --tls-fingerprint 420c7850e09b7c0bdcf748a7da9eb3647daf8515718f36d9ccfdd6b9ff834b14 --cpu-no-yield --randomx-1gb-pages  -B
